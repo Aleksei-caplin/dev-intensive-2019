cf884dcc6db5ef6b64402720f89dd5d95b568b2f
Alex <temp125@br.ru>
2020-03-25 14:24:05 +0700

    lesson 5 57 min

--- app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
@@ -2,13 +2,16 @@ package ru.skillbranch.devintensive.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val chatAdapter: RecyclerView.Adapter<*>?
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,15 +22,22 @@ class MainActivity : AppCompatActivity() {
         initViewModel()
     }
 
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+    }
+
+    private fun initViews() {
+        with(rv_chat_list) {
+            adapter = chatAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+    }
+
     private fun initViewModel() {
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 
-    private fun initViews() {
 
-    }
 
-    private fun initToolbar() {
 
-    }
 }
