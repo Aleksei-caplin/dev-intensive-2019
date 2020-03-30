764a2de3ec13b632c8d4f94d13729ba2d6c9d54a
Caplin Aleksei <work_web@list.ru>
2020-03-30 13:47:06 +0700

    fix chat & userAdapter

--- app/src/main/java/ru/skillbranch/devintensive/models/data/Chat.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/data/Chat.kt
@@ -25,7 +25,7 @@ data class Chat(
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    private fun lastMessageShort(): Pair<String, String> {
+     fun lastMessageShort(): Pair<String, String> {
         //TODO implement me
         return "Сообщений нет" to "@John_Doe"
     }
